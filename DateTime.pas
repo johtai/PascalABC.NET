5e8930d8f1c6009dc66000305903720d49cb4316
johtai <johtai@yandex.ru>
2021-10-21 08:31:30 +0300

    add .pas

--- DateTime.pas
+++ DateTime.pas
@@ -1,4 +1,8 @@
-﻿unit DateTime;
-begin
-  
+﻿begin
+  function IsLeapYear(year: integer): integer;
+  begin
+    result := False;
+    if ((year mod 4 = 0) and (year mod 100 <> 0)) 
+        or (year mod 400 = 0) then result := True;
+  end;
 end.
